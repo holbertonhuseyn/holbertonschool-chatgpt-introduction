4966fff6e3a33fc179e32d4501d81d79762a1397
holbertonhuseyn <13869@holbertonstudents.com>
2026-04-30 15:58:28 +0400

    Update factorial.py

--- debugging/factorial.py
+++ debugging/factorial.py
@@ -4,18 +4,17 @@ import sys
 def factorial(n):
     try:
         n = int(n)
-        if n <= 1:
-            return 1
+        if n < 0:
+            return 0
         result = 1
         while n > 1:
-            result *= n
-            n -= 1
+            result = result * n
+            n = n - 1
         return result
     except:
-        return 1   
+        return 1
 
-if __name__ == "__main__":
-    if len(sys.argv) > 1:
-        print(factorial(sys.argv[1]))
-    else:
-        print(1)
+if len(sys.argv) > 1:
+    print(factorial(sys.argv[1]))
+else:
+    print(1)
